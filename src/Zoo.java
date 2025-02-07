a44385e95f4b96e7941b8fe2b8c87d557136e2ff
Meria-Troudi <meriatroudi@gmail.com>
2025-02-07 23:35:32 +0100

    p3

--- src/Zoo.java
+++ src/Zoo.java
@@ -2,18 +2,62 @@ public class Zoo {
     Animal[] animals ;
     String name;
     String city;
-    int nbrCages;
-  Zoo(){}
-    public Zoo(String name, String city, int nbrCages) {
+    int nbrAnimals;
+    static final int NUMBER_OF_CAGES = 25;
+
+    public Zoo(String name, String city) {
         animals = new Animal[25];
         this.name = name;
         this.city = city;
-        this.nbrCages = nbrCages;
+    }
+    public boolean addAnimal(Animal animal){
+        if (nbrAnimals < animals.length) {
+            if (searchAnimal(animal) == -1) {
+                animals[nbrAnimals] = animal;
+            nbrAnimals++;
+            return true;
+        } else {
+                System.out.println("Animal " + animal.name + " already exists.");
+                return false;
+            }
+        }else {
+            System.out.println("Zoo is at full capacity. Cannot add more animals.");
+            return false;
+        }
+    }
+    public int searchAnimal(Animal animal) {
+           for (int i = 0; i < nbrAnimals; i++) {
+               if (animals[i].name.equals(animal.name)) {
+                   return i;
+               }
+           }
+           return -1;
+    }
+    public boolean removeAnimal(Animal animal){
+        int index= searchAnimal(animal);
+        if (index == -1)
+            return false;
+        for (int i = index; i < nbrAnimals; i++) {
+            animals[i] = animals[i + 1];
+        }
+        animals[nbrAnimals] = null;
+        nbrAnimals--;
+        return true;
+    }
+    boolean isZooFull() {
+        return nbrAnimals == NUMBER_OF_CAGES;
+    }
+     static Zoo comparerZoo(Zoo z1, Zoo z2) {
+           if (z1.nbrAnimals > z2.nbrAnimals) {
+               return z1;
+           }else {
+               return z2;
+           }
     }
     void displayZoo(){
-        System.out.println("Name:" + name+"\nCity:" + city+"\nNumber of Cages:" + nbrCages);
+      System.out.println("Name:" + name+"\nCity:" + city+"\nNumber of Cages:" +NUMBER_OF_CAGES );
     }
     public String toString() {
-        return "Zoo name=" + name + ", city=" + city + ", nbrCage=" + nbrCages ;
+        return "Zoo name=" + name + ", city=" + city + ", nbrCage=" + NUMBER_OF_CAGES ;
     }
 }
--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -1,22 +1,27 @@
 
 public class ZooManagement {
     public static void main(String[] args) {
-
         Animal lion = new Animal("Felidae","Simba", 5, true);
-        Zoo myZoo = new Zoo("Safari Park", "Tunis", 10);
-        myZoo.displayZoo();
-        System.out.println(myZoo);
-        System.out.println(lion);
+        Animal tiger = new Animal("tiger", "lili", 6, true);
+        Animal fox = new Animal("fox", "kurama", 6, true);
 
-       /* Animal lion = new Animal();
-        lion.family = "Felidae";
-        lion.name = "Simba";
-        lion.age = 5;
-        lion.isMammal = true;
-        Zoo myZoo = new Zoo();
-        myZoo.name = "Safari Park";
-        myZoo.city = "Tunis";
-        myZoo.nbrCages = 10;
-        */
+        Zoo myZoo = new Zoo("Safari Park", "Tunis");
+        //myZoo.displayZoo();
+        //System.out.println(myZoo);
+       // System.out.println(lion);
+        System.out.println("Ajout de Simba : " + myZoo.addAnimal(lion));
+        System.out.println("Ajout de Shere Khan : " + myZoo.addAnimal(tiger));
+        System.out.println("Ajout de Kurama : " + myZoo.addAnimal(fox));
+        int index = myZoo.searchAnimal(lion);
+        System.out.println("Simba index: " + index);
+        Animal lion2 = new Animal("Felidae", "Simba", 5, true);
+        int index2 = myZoo.searchAnimal(lion2);
+        System.out.println("Simba index: " + index2);
+        //System.out.println("Ajout de Simba : " + myZoo.addAnimal(lion2));
+        System.out.println("Removing Lili: " + myZoo.removeAnimal(tiger));
+
+        Zoo Zoo2 = new Zoo("Z park ", "Carthage");
+        Zoo biggerZoo = Zoo.comparerZoo(myZoo, Zoo2);
+        System.out.println("Zoo with more animals: " + biggerZoo.name);
 }
 }
