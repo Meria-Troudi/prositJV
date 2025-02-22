b7bbd4129ce1d05a40b85494b0d8f826266a517d
Meria-Troudi <meriatroudi@gmail.com>
2025-02-22 17:42:59 +0100

    p5

---
+++ src/tn/esprit/gestionzoo/entities/Terrestrial.java
@@ -0,0 +1,21 @@
+package tn.esprit.gestionzoo.entities;
+
+public class Terrestrial extends Animal {
+    private int nbrLegs;
+    public Terrestrial() {}
+
+    public Terrestrial(String family, String name, int age, boolean isMammal,int nbrLegs){
+        super(family, name, age, isMammal);
+        this.nbrLegs = nbrLegs;
+    }
+    public void setNbrLegs(int nbrLegs) {
+        this.nbrLegs = nbrLegs;
+    }
+    public int getNbrLegs() {
+        return nbrLegs;
+    }
+    @Override
+    public String toString() {
+        return super.toString() + ", Number of Legs: " + nbrLegs;
+    }
+}
--- src/tn/esprit/gestionzoo/main/ZooManagement.java
+++ src/tn/esprit/gestionzoo/main/ZooManagement.java
@@ -3,24 +3,24 @@ import tn.esprit.gestionzoo.entities.*;
 
 public class ZooManagement {
     public static void main(String[] args) {
-        Animal lion = new Animal("Felidae","Simba", 5, true);
-        Animal tiger = new Animal("tiger", "lili", 6, true);
-        Animal fox = new Animal("fox", "kurama", 6, true);
 
-        Zoo myZoo = new Zoo("Safari Park", "Tunis");
-        myZoo.displayZoo();
-        System.out.println("Ajout de lion : " + myZoo.addAnimal(lion));
-        System.out.println("Ajout de tiger : " + myZoo.addAnimal(tiger));
-        System.out.println("Ajout de fox : " + myZoo.addAnimal(fox));
+        Aquatic aquatic = new Aquatic();
+        Terrestrial terrestrial = new Terrestrial();
+        Dolphin d = new Dolphin();
+        Penguin p= new Penguin();
 
-        System.out.println("lion index: " +  myZoo.searchAnimal(lion));
-        Animal lion2 = new Animal("Felidae", "Simba", 5, true);
-        System.out.println("Simba index: " + myZoo.searchAnimal(lion2));
-        System.out.println("Ajout de Simba : " + myZoo.addAnimal(lion2));
-        System.out.println("Removing tiger: " + myZoo.removeAnimal(tiger));
+        Terrestrial tAn = new Terrestrial("Mammal", "Lion", 10, true, 4);
+        Aquatic aAn = new Aquatic("Fish", "Shark", 15, false, "Ocean");
+        Dolphin d1= new Dolphin("Mammal","Dolphin",15,true,"ocean",60.0f);
+        Penguin p1= new Penguin("Mammal","Penguin",15,true,"ocean",50.0f);
 
-        Zoo Zoo2 = new Zoo("Z park ", "Carthage");
-        Zoo biggerZoo = Zoo.comparerZoo(myZoo, Zoo2);
-        System.out.println("Zoo with more animals: " + biggerZoo.getName());
+        System.out.println(tAn);
+        System.out.println(aAn);
+        System.out.println(d1);
+        System.out.println(p1);
+
+        aAn.swim();
+        d1.swim();
+        p1.swim();
 }
 }
